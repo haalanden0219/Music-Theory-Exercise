9d86a19a6729c082d3a391292904e4b2d8b19cb5
Evan <haalanden@vcu.edu>
2024-02-27 17:29:27 -0500

    Added new scale, organized test, updated readme

--- DSMTT.py
+++ DSMTT.py
@@ -1,7 +1,6 @@
 #Extension of project inspired by Connor Kreitzman
 #Evan Haaland & Connor Kreitzman
 
-
 import random
 import argparse
 
@@ -9,7 +8,7 @@ def get_major_scale(root_note):
     notes_sharp = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
     root_index = notes_sharp.index(root_note)
     major_scale_intervals = [0, 2, 4, 5, 7, 9, 11]
-    major_scale = [notes_sharp[(root_index + interval) % len(notes_sharp)] for interval in major_scale_intervals] # wprk out algorithm
+    major_scale = [notes_sharp[(root_index + interval) % len(notes_sharp)] for interval in major_scale_intervals] 
     return major_scale
 
 def get_minor_scale(root_note): 
@@ -18,6 +17,49 @@ def get_minor_scale(root_note):
     minor_scale_intervals = [0, 2, 3, 5, 7, 8, 10]
     minor_scale = [notes_sharp[(root_index + interval) % len(notes_sharp)] for interval in minor_scale_intervals]
     return minor_scale
+
+def get_harmonic_minor_scale(root_note):
+    notes_sharp = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    root_index = notes_sharp.index(root_note)
+    harmonic_minor_scale_interval = [0, 2, 3, 5, 7, 8, 11]
+    harmonic_minor_scale = [notes_sharp[(root_index + interval) % len(notes_sharp)] for interval in harmonic_minor_scale_interval] 
+    return harmonic_minor_scale
+
+def user_identify_scale(scale_under_test):
+    user_built_scale = []
+    success_counter = 0
+    failure_counter = 0
+    for note in scale_under_test:
+            if success_counter == 0:
+                user_answer = input("What is the root note in the scale? ")
+            elif success_counter == 1:
+                user_answer = input("What is the 2nd note in the scale? ")
+            elif success_counter == 2:
+                user_answer = input("What is the 3rd note in the scale? ")
+            elif success_counter == 3:
+                user_answer = input("What is the 4th note in the scale? ")
+            elif success_counter == 4:
+                user_answer = input("What is the 5th note in the scale? ")
+            elif success_counter == 5:
+                user_answer = input("What is the 6th note in the scale? ")
+            elif success_counter == 6:
+                user_answer = input("What is the 7th note in the scale? ")
+            if (user_answer.upper() == note ):
+                user_built_scale.append(note)
+                for sub_note in user_built_scale:
+                    print(sub_note.upper(), end= ' ')
+                print()
+                print(user_answer.upper() , "is correct!")
+                success_counter = success_counter + 1
+                failure_counter = 0
+            else: 
+                print(user_answer, "is NOT correct!")
+                failure_counter = failure_counter + 1
+                print("The correct notes are:")
+                for note in scale_under_test:
+                    print(note.upper(), end= ' ')
+                print("\nExiting Program")
+                exit()
 
 def print_scales(note, scale):
     if scale.upper() == 'MAJOR':
@@ -30,8 +72,11 @@ def print_scales(note, scale):
         created_scale = get_minor_scale(note.upper())
         for note in created_scale:
             print(note, end= ' ')
-    elif scale.upper() == 'Harmonic Minor':
-        created_scale = []
+    elif scale.upper() == 'HARMONIC MINOR':
+        print(note.upper() , "Harmonic Minor Scale")
+        created_scale = get_harmonic_minor_scale(note.upper())
+        for note in created_scale:
+            print(note, end= ' ')
     elif scale.upper() == 'Melodic Minor':
         created_scale = []
 
@@ -40,92 +85,77 @@ def test_scale(note, scale):
     if scale.upper() == 'MAJOR':
         print(note.upper(), "Major Scale")
         created_scale = get_major_scale(note.upper())
-        for note in created_scale:
-            user_answer = input("What is the next note in the scale? ")
-            if (user_answer.upper() == note ):
-                print(user_answer , "is correct!")
-            else:
-                print(user_answer, "is NOT correct!")
-                for note in created_scale:
-                    print(note.upper(), end= ' ')
-                print("Exiting Program")
-                exit()
+        user_identify_scale(created_scale)
     elif scale.upper() == 'MINOR': 
         print(note.upper() , "Minor Scale")
         created_scale = get_minor_scale(note.upper())
-        for note in created_scale:
-            user_answer = input("What is the next note in the scale? ")
-            if (user_answer.upper() == note ):
-                print(user_answer.upper() , "is correct!")
-            else:
-                print(user_answer.upper(), "is NOT correct!")
-                for note in created_scale:
-                    print(note.upper(), end= ' ')
-                print("Exiting Program")
-                exit()
-    elif scale.upper() == 'Harmonic Minor':
-        created_scale = []
+        user_identify_scale(created_scale)
+    elif scale.upper() == 'HARMONIC MINOR':
+        print(note.upper(), "Harmonic Minor Scale")
+        created_scale = get_harmonic_minor_scale(note.upper())
+        user_identify_scale(created_scale)
     elif scale.upper() == 'Melodic Minor':
         created_scale = []
 
 def main():
-    types_of_scales = ['MAJOR', 'MINOR'] 
+    types_of_scales = ['MAJOR', 'MINOR' , 'HARMONIC MINOR'] 
     notes_sharp = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    try:
+        parser = argparse.ArgumentParser(description="Diatonic Scale Music Theory Tool: A useful tool to learn diatonic scales")
+        parser.add_argument("--note", help="Name of Note")
+        parser.add_argument("--scale", help="Name of Scale Type")
+        parser.add_argument("-t" ,"--test", action="store_true", help="Enables test mode")
 
-    parser = argparse.ArgumentParser(description="Diatonic Scale Music Theory Tool: A useful tool to learn diatonic scales")
-    parser.add_argument("--note", help="Name of Note")
-    parser.add_argument("--scale", help="Name of Scale Type")
-    parser.add_argument("-t" ,"--test", action="store_true", help="Enables test mode")
+        args = parser.parse_args()
+    
+        if args.note and args.scale:
+            try: 
+                if ((args.note.upper() in notes_sharp) and (args.scale.upper() in types_of_scales)): 
+                    if args.test:
+                        test_scale(args.note , args.scale)
+                    else:
+                        print_scales(args.note , args.scale)
+                else:
+                    print("Invalid Note and/or Scale")
+            except Exception as e:
+                print("An exception occurred:", str(e))
 
-    args = parser.parse_args()
+        elif args.note:        
+            try: 
+                if (args.note.upper() in notes_sharp ): 
+                    random_scale = random.choice(types_of_scales)
+                    if args.test:
+                        test_scale(args.note , random_scale)
+                    else:
+                        print_scales(args.note , random_scale)
+                else:
+                    print(args.note, "is not a valid note")
+            
+            except Exception as e:
+                print("An exception occurred:", str(e))
 
-    if args.note and args.scale:
-        try: 
-            if ((args.note.upper() in notes_sharp) and (args.scale.upper() in types_of_scales)): 
-                if args.test:
-                    test_scale(args.note , args.scale)
+        elif args.scale: 
+            try: 
+                if (args.scale.upper() in types_of_scales ): 
+                    random_note = random.choice(notes_sharp)
+                    if args.test:
+                        test_scale(random_note , args.scale)
+                    else:
+                        print_scales(random_note , args.scale)
                 else:
-                    print_scales(args.note , args.scale)
-            else:
-                print("Invalid Note and/or Scale")
-        except Exception as e:
-            print("An exception occurred:", str(e))
-    
-    elif args.note:        
-        try: 
-            if (args.note.upper() in notes_sharp ): 
-                random_scale = random.choice(types_of_scales)
-                if args.test:
-                    test_scale(args.note , random_scale)
-                else:
-                    print_scales(args.note , random_scale)
-            else:
-                print(args.note, "is not a valid note")
-        
-        except Exception as e:
-            print("An exception occurred:", str(e))
-    
-    elif args.scale: 
-        try: 
-            if (args.scale.upper() in types_of_scales ): 
-                random_note = random.choice(notes_sharp)
-                if args.test:
-                    test_scale(random_note , args.scale)
-                else:
-                    print_scales(random_note , args.scale)
-            else:
-                print(args.scale, "is not a valid scale")
-        
-        except Exception as e:
-            print("An exception occurred:", str(e))
-    
-    else:
-        random_note = random.choice(notes_sharp)
-        random_scale = random.choice(types_of_scales)
-        if args.test:
-            test_scale(random_note, random_scale)
+                    print(args.scale, "is not a valid scale")
+            
+            except Exception as e:
+                print("An exception occurred:", str(e))
+
         else:
-            print_scales(random_note, random_scale)
-
+            random_note = random.choice(notes_sharp)
+            random_scale = random.choice(types_of_scales)
+            if args.test:
+                test_scale(random_note, random_scale)
+            else:
+                print_scales(random_note, random_scale)
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected. Exiting program.")
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ## Description
 
-Project created to help a user be able to quickly identify the notes of diatonic scales to help them learn music theory. The current version has the user generate a major or minor scale by choice or random and be able to see the notes of that scale. We recently added a testing feature mode where the user is required to enter the notes by hand. We currently on support major and minor scales but are hoping to add more types. We currently have created a command line tool but want to design a gui executable in the future. The project is under development and more features will be added overtime.
+Project created to help a user be able to quickly identify the notes of diatonic scales to help them learn music theory. The current version has the user generate a major, minor or harmonic minor scale by choice or random and be able to see the notes of that scale. We recently added a testing feature mode where the user is required to enter the notes by hand. We currently only support major, minor, and harmonic diatonic using sharp notes. We currently have created a command line tool but want to design a gui executable in the future. The project is under development and more features will be added overtime.
 
+### Testing
+One of the main goals we wanted with the tool is the ability to test one's knowlege of being able to figure out the notes of the scale by memory. This is to help someone understand the intervals of how the scales are made. For our test we currently have the user select test mode and enter the scale and/or note they want to attempt. If no note or scale is selected they will get a random test. Currently, the user only gets one attempt at guessing each note. 
 ## Getting Started
 
 ### Dependencies
@@ -21,8 +23,10 @@ Project created to help a user be able to quickly identify the notes of diatonic
 ``` python DSMTT.py ``` </br>
 - Running with the note and/or --scale commands will let you choose a scale and root note. </br>
 ``` python DSMTT.py --note C --scale Major``` </br>
+> **Note:** The argument for harmonic minor scale must be entered in quotes like "Harmonic Minor" to be registered as the whole argument. </br>
 - Running with the -t flag will put you into test mode. </br>
 ``` python DSMTT.py -t ``` </br>
+ 
 
 ## Help
 - Run code below for further help </br>
@@ -32,6 +36,7 @@ Project created to help a user be able to quickly identify the notes of diatonic
 - Create a gui executable for ease of access for non-programmers.
 - Add a feature to test the 7 modes along side the scales.
 - Implement an option to use flat notes instead of sharp.
+- Implement an option where the user can modify testing parameters
 
 
 ## Authors
